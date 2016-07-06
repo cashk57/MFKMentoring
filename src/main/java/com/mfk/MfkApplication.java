a5fcb3c04658f2097c5fb0546cdd3a99317fa76f
Ronnie King <Ronnie King>
2016-07-06 09:50:08 -0400

    first commit

---
+++ src/main/java/com/mfk/MfkApplication.java
@@ -0,0 +1,12 @@
+package com.mfk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfkApplication.class, args);
+	}
+}
